907860cb2f8473bdbee7b1265d643e02aeeffd63
chltnqud0 <50440687+chltnqud0@users.noreply.github.com>
2025-02-20 14:22:25 +0900

    Time: 94 ms (95.56%), Space: 31.4 MB (69.72%) - LeetHub

--- 1235-maximum-profit-in-job-scheduling/1235-maximum-profit-in-job-scheduling.py
+++ 1235-maximum-profit-in-job-scheduling/1235-maximum-profit-in-job-scheduling.py
@@ -5,7 +5,7 @@ class Solution:
         jobs = sorted(zip(startTime, endTime, profit), key=lambda v: v[1])
         dp = [[0, 0]]
         for s, e, p in jobs:
-            i = bisect.bisect(dp, [s + 1]) - 1
+            i = bisect.bisect(dp, [s, float(inf)]) - 1
             if dp[i][1] + p > dp[-1][1]:
                 dp.append([e, dp[i][1] + p])
         return dp[-1][1]
