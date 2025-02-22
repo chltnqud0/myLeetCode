aec789210a8601eb6ba638354339fc325ee21a29
chltnqud0 <50440687+chltnqud0@users.noreply.github.com>
2025-02-22 13:59:24 +0900

    Time: 0 ms (100%), Space: 18 MB (14.81%) - LeetHub

--- 0031-next-permutation/0031-next-permutation.py
+++ 0031-next-permutation/0031-next-permutation.py
@@ -8,9 +8,7 @@ class Solution:
             if nums[i] > nums[i-1]:
                 f = [ (a,b) for a,b, in enumerate(nums) if b > nums[i-1] and a > i -1]
                 index = min(f, key= lambda x: (x[1] - nums[i-1], -x[0]))
-                print(index)
                 temp = nums.pop(index[0])
-                print(temp)
                 nums[i-1:] = sorted(nums[i-1:])
                 nums.insert(i-1, temp)
                 break
